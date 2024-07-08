c53f6c8b8561fcb34c61f06eca7e071eb8b9fafa
mike connor <mike@mikeconnor.tech>
2024-07-08 15:25:56 -0500

    box

--- src/lit-layout-components/Box/Box.ts
+++ src/lit-layout-components/Box/Box.ts
@@ -1,6 +1,6 @@
 import { html, css, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
-
+//
 import { styleMap } from "lit/directives/style-map.js";
 
 // const invertValue = css`invert(100%)`;
@@ -15,48 +15,36 @@ export class Box extends LitElement {
     css`
       :host {
         display: block;
-
         outline: 0.125rem solid transparent;
         outline-offset: -0.125rem;
-
         color: inherit;
         background-color: inherit;
-        ::slotted(*) {
-          color: inherit;
-          background-color: inherit;
-        }
       }
     `,
   ];
 
-  // static properties = { padding: { type: String } };
-  // padding = "10px";
-
   @property({ type: String })
-  padding = "8px";
+  accessor padding = "var(--s1)";
 
-  @property({ type: String })
-  borderWidth = "1px";
+  @property()
+  accessor borderWidth = "1px";
 
   @property({ type: Boolean })
-  invert = false;
+  accessor invert = false;
 
   render() {
     const styles = {
       border: `${this.borderWidth} solid`,
       padding: this.padding,
-      color: ` var(--text-3)`,
-      backgroundColor: `var(--surface-3)`,
-
-      filter: this.invert ? "invert(100%)" : "",
+      filter: this.invert ? `invert(100%)` : "",
     };
 
-    return html`<div style=${styleMap(styles)}><slot /></div>`;
+    return html`<div style=${styleMap(styles)}><slot></slot></div>`;
   }
 }
 
-declare global {
-  interface HTMLElementTagNameMap {
-    "box-l": Box;
-  }
-}
+// declare global {
+//   interface HTMLElementTagNameMap {
+//     "box-l": Box;
+//   }
+// }
